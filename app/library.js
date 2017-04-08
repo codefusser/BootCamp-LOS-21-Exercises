dfb75d30fcab6661ee7f3b67f5590fdc5182c8d4
Kingsley Uchunor <kizucom@gmail.com>
2017-04-08 04:23:47 +0100

    Add files via upload
    This is a unitest framework that could be implemented with both Chai and Jasmine. In its branches are project demonstrations for this purpose.

---
+++ app/library.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = {
+
+ /* Find the average of three integers */
+ computeAverage: function(num1, num2, num3) {
+   var average;
+   return average;
+ },
+ 
+ /* Find the factorial of an integer */
+ computeFactorial: function(num) {
+   var factorial;
+   return factorial;
+ },
+
+ /* Convert a given Celcius temperature to Fahrenheit */
+ convertTempCtoF: function(cTemp) {
+   var fTemp;
+   return fTemp;
+ },
+
+ /* Convert a given Fahrenheit temperature to Celcius */
+ convertTempFtoC: function(fTemp) {
+   var cTemp;
+   return cTemp;
+ }
+
+}
